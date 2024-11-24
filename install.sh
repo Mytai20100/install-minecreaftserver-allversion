3255dd8a5db6ccebd40c0b24f9431500c706977d
Mytai20100 <142713298+Mytai20100@users.noreply.github.com>
2024-11-24 17:02:59 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,139 +1,224 @@
 #!/bin/bash
 
-# Clear the screen
+# Clear the screen and set up functions for cursor handling
 clear
+hide_cursor() { echo -ne "\033[?25l"; }
+show_cursor() { echo -ne "\033[?25h"; }
 
-# LEAVE CREDITS
-echo "
+# Print header
+print_header() {
+    echo "
 #######################################################################################
-#
-#                                  Minecraft
-#
-#                           Copyright (C) 2023 - 2024
-#
+#                                                                                     #
+#                                  Minecraft                                          #
+#                                                                                     #
+#                           Copyright (C) 2023 - 2024                                 #
+#                                                                                     #
 #######################################################################################"
+}
 
-# Prompt user for server folder name
+if ! command -v java &> /dev/null; then
+    echo "Java is not installed. Proceeding to select and install JDK."
+
+    jdk_versions=("openjdk-8-jdk" "openjdk-11-jdk" "openjdk-16-jdk" "openjdk-17-jdk" "openjdk-21-jdk")
+    selected_jdk=3 # Default to JDK 17
+
+    jdk_options=("OpenJDK 8" "OpenJDK 11" "OpenJDK 16" "OpenJDK 17" "OpenJDK 21")
+    while true; do
+        clear
+        echo "Select the OpenJDK version you want to install:"
+        for i in "${!jdk_options[@]}"; do
+            if [ $i -eq $selected_jdk ]; then
+                echo -e "\033[1m> ${jdk_options[$i]}\033[0m"
+            else
+                echo "  ${jdk_options[$i]}"
+            fi
+        done
+        read -rsn1 key
+        case "$key" in
+            $'\x1b')
+                read -rsn2 -t 0.1 key
+                case "$key" in
+                    "[A")
+                        ((selected_jdk--))
+                        if [ $selected_jdk -lt 0 ]; then
+                            selected_jdk=$((${#jdk_options[@]} - 1))
+                        fi
+                        ;;
+                    "[B")
+                        ((selected_jdk++))
+                        if [ $selected_jdk -ge ${#jdk_options[@]} ]; then
+                            selected_jdk=0
+                        fi
+                        ;;
+                esac
+                ;;
+            "")
+                jdk_version=${jdk_versions[$selected_jdk]}
+                break
+                ;;
+        esac
+    done
+
+
+    if ! dpkg -l | grep -q "$jdk_version"; then
+        sudo apt-get update
+        sudo apt-get install -y "$jdk_version"
+    fi
+else
+    echo "Java is already installed. Skipping JDK selection and installation."
+fi
+
+
+options=(
+    "Vanilla"
+    "Spigot (using BuildTools)"
+    "Paper"
+    "Purpur"
+)
+selected=0
+
+print_menu() {
+    clear
+    print_header
+    echo "Use UP/DOWN arrow keys to navigate and press ENTER to select."
+    for i in "${!options[@]}"; do
+        if [ $i -eq $selected ]; then
+            echo -e "\033[1m> ${options[$i]}\033[0m" # Highlight the selected option
+        else
+            echo "  ${options[$i]}"
+        fi
+    done
+}
+
+# Trap to restore cursor on exit
+trap show_cursor EXIT
+hide_cursor
+
+# Menu navigation loop
+while true; do
+    print_menu
+    read -rsn1 key
+    case "$key" in
+        $'\x1b') # Escape sequence
+            read -rsn2 -t 0.1 key
+            case "$key" in
+                "[A") # Arrow up
+                    ((selected--))
+                    if [ $selected -lt 0 ]; then
+                        selected=$((${#options[@]} - 1))
+                    fi
+                    ;;
+                "[B") # Arrow down
+                    ((selected++))
+                    if [ $selected -ge ${#options[@]} ]; then
+                        selected=0
+                    fi
+                    ;;
+            esac
+            ;;
+        "") # Enter key
+            server_choice=$selected
+            break
+            ;;
+    esac
+done
+
+# Prompt for server folder
 read -p "Enter a name for your Minecraft server folder: " server_folder
 mkdir -p "$server_folder"
 cd "$server_folder" || exit
 
-# Prompt user to choose server type
-echo "Select the type of Minecraft server to install:"
-echo "1) Vanilla"
-echo "2) Spigot (using BuildTools)"
-echo "3) Paper"
-echo "4) Purpur"
-read -p "Enter the number corresponding to the server type (default is Vanilla): " server_choice
-server_choice=${server_choice:-1} # Default to Vanilla
-
-# Prompt user for specific server version
+# Prompt for server version
 read -p "Enter the server version you want to install (e.g., 1.20.1): " server_version
 
-# Determine the download URL based on user selection
+# Determine the download URL based on menu selection
 case $server_choice in
-    1) 
-        server_url="https://piston-data.mojang.com/v1/objects/5b868151bd02b41319f54c8d4061b8cae84e665c/server.jar" 
-        server_name="Vanilla"
-        ;;
-    2)
-        # Download BuildTools.jar
-        echo "Downloading BuildTools.jar..."
-        wget -O BuildTools.jar "https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar"
-        
-        # Run BuildTools to compile Spigot
-        echo "Running BuildTools to compile Spigot..."
-        java -jar BuildTools.jar --rev "$server_version"
-        
-        # Check if the Spigot jar is created
-        if [[ -f "spigot-$server_version.jar" ]]; then
-            server_name="Spigot"
-            server_url="spigot-$server_version.jar"
-        else
-            echo "Error: Failed to compile Spigot. Please check your version and try again."
-            exit 1
-        fi
-        ;;
-    3) 
-        server_url="https://api.papermc.io/v2/projects/paper/versions/${server_version}/builds/latest/downloads/paper-${server_version}-latest.jar" 
-        server_name="Paper"
-        ;;
-    4) 
-        server_url="https://api.purpurmc.org/v2/purpur/${server_version}/latest/download" 
-        server_name="Purpur"
-        ;;
-    *) 
-        echo "Invalid option, defaulting to Vanilla."
+    0)
         server_url="https://piston-data.mojang.com/v1/objects/5b868151bd02b41319f54c8d4061b8cae84e665c/server.jar"
         server_name="Vanilla"
         ;;
+    1)
+        wget -O BuildTools.jar "https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar"
+        java -jar BuildTools.jar --rev "$server_version"
+        server_name="Spigot"
+        server_url="spigot-$server_version.jar"
+        ;;
+    2)
+        server_url="https://api.papermc.io/v2/projects/paper/versions/${server_version}/builds/latest/downloads/paper-${server_version}-latest.jar"
+        server_name="Paper"
+        ;;
+    3)
+        server_url="https://api.purpurmc.org/v2/purpur/${server_version}/latest/download"
+        server_name="Purpur"
+        ;;
 esac
 
-# Download the server jar if not using BuildTools
-if [[ $server_choice -ne 2 ]]; then
-    echo "Downloading $server_name server (version $server_version) from $server_url..."
-    # Check if wget is installed, use curl if wget is not found
-    if command -v wget &> /dev/null; then
-        wget -O server.jar "$server_url"
-    else
-        echo "wget not found, using curl..."
-        if command -v curl &> /dev/null; then
-            curl -L -o server.jar "$server_url"
-        else
-            echo "Error: Neither wget nor curl is installed. Please install one of them to proceed."
-            exit 1
-        fi
-    fi
+# Download the server jar
+if [[ $server_choice -ne 1 ]]; then
+    wget -O server.jar "$server_url"
 fi
 
-# Check and install the selected JDK version if necessary
-echo "Select the OpenJDK version you want to install:"
-echo "1) OpenJDK 8"
-echo "2) OpenJDK 11"
-echo "3) OpenJDK 16"
-echo "4) OpenJDK 17"
-echo "5) OpenJDK 21"
-read -p "Enter the number corresponding to the JDK version (default is 17): " jdk_choice
-jdk_choice=${jdk_choice:-4} # Default to JDK 17
 
-case $jdk_choice in
-    1) jdk_version="openjdk-8-jdk" ;;
-    2) jdk_version="openjdk-11-jdk" ;;
-    3) jdk_version="openjdk-16-jdk" ;;
-    4) jdk_version="openjdk-17-jdk" ;;
-    5) jdk_version="openjdk-21-jdk" ;;
-    *) echo "Invalid option, defaulting to OpenJDK 17."; jdk_version="openjdk-17-jdk" ;;
-esac
-
-# Check if the selected JDK is already installed
 if ! dpkg -l | grep -q "$jdk_version"; then
-    echo "Installing $jdk_version..."
     sudo apt-get update
     sudo apt-get install -y "$jdk_version"
-else
-    echo "$jdk_version is already installed, skipping installation."
 fi
 
-# Ask the user if they accept the Minecraft EULA
-read -p "Do you accept the Minecraft EULA? (Type 'yes' or 'y' to accept): " eula_acceptance
-
-# Check if the user accepted the EULA
+# Accept EULA and configure server
+read -p "Do you accept the Minecraft EULA? (yes/no): " eula_acceptance
 if [[ "$eula_acceptance" =~ ^[Yy][Ee][Ss]?$ ]]; then
-    # Ask the user for the amount of RAM to allocate
-    read -p "How much RAM would you like to allocate to the Minecraft server (in GB)? " ram_amount
-
-    # Save Java command with user's response in a start.sh script
-    java_command="java -Xms${ram_amount}G -Xmx${ram_amount}G -jar server.jar nogui"
-    echo "#!/bin/bash" > start.sh
-    echo "$java_command" >> start.sh
-    chmod +x start.sh
-
-    # Modify the eula.txt file to set 'true'
     echo "eula=true" > eula.txt
-
-    # Launch the Minecraft server
-    echo "Starting $server_name server with ${ram_amount}G RAM..."
+    read -p "Enter RAM to allocate (in GB): " ram_amount
+    echo "#!/bin/bash" > start.sh
+    echo "java -Xms${ram_amount}G -Xmx${ram_amount}G -jar server.jar nogui" >> start.sh
+    chmod +x start.sh
     ./start.sh
 else
-    echo "You must accept the Minecraft EULA to proceed. Aborting."
+    echo "You must accept the EULA to proceed. Exiting."
+    exit 1
 fi
+
+GREEN="\033[0;32m"
+YELLOW="\033[0;33m"-e
+RED="\033[0;31m"
+RESET="\033[0m"
+CYAN="\033[0;36m"
+WHITE="\033[0;37m"
+RESET_COLOR="\033[0m"
+
+# Lấy thông tin hệ thống
+OS_VERSION=$(lsb_release -ds 2>/dev/null || echo "N/A")
+CPU_NAME=$(lscpu | awk -F: '/Model name:/ {print $2}' | sed 's/^ //')
+CPU_ARCH=$(uname -m)
+CPU_USAGE=$(top -bn1 | awk '/Cpu\(s\)/ {print $2 + $4}')
+TOTAL_RAM=$(free -h --si | awk '/^Mem:/ {print $2}')
+USED_RAM=$(free -h --si | awk '/^Mem:/ {print $3}')
+DISK_SPACE=$(df -h / | awk 'NR==2 {print $2}')
+USED_DISK=$(df -h / | awk 'NR==2 {print $3}')
+PORTS=$(ss -tunlp | wc -l)
+IP_ADDRESS=$(hostname -I | awk '{print $1}')
+
+
+display_gg() {
+  echo -e "${WHITE}___________________________________________________${RESET_COLOR}"
+  echo -e "           ${CYAN}-----> Mission Completed ! <----${RESET_COLOR}"
+}
+
+display_version() {
+  echo -e "${WHITE}_______________________________________________________________________${RESET_COLOR}"
+  echo -e "${CYAN}OS:${RESET} $OS_VERSION"
+  echo -e "${CYAN}CPU:${RESET} $CPU_NAME [$CPU_ARCH]"
+  echo -e "${CYAN}Used CPU:${RESET} ${CPU_USAGE}%"
+  echo -e "${GREEN}RAM:${RESET} $USED_RAM / $TOTAL_RAM"
+  echo -e "${YELLOW}Disk:${RESET} $USED_DISK / $DISK_SPACE"
+  echo -e "${RED}Ports:${RESET} $PORTS"
+  echo -e "${RED}IP:${RESET} $IP_ADDRESS"
+  echo -e "${WHITE}_______________________________________________________________________${RESET_COLOR}"
+}
+
+clear
+display_version
+echo  ""
+display_gg
+
